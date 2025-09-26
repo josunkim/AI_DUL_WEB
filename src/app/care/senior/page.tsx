aec9a940b4b05cebc0c771fad5af67e5be345c1c
김조순 <whtns611@naver.com>
2025-09-26 12:47:01 +0900

    fix : 빌드에서

--- src/app/care/senior/page.tsx
+++ src/app/care/senior/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import ResponsiveContainer from '@/shared/ResponsiveContainer';
 import MainIntro from './_components/MainIntro';
 import SeniorService from './_components/SeniorService';
 
