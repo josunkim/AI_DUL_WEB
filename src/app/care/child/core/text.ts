57cddf7ba0f47d2f73b03aa43025bc1f81382637
김조순 <whtns611@naver.com>
2025-10-02 18:09:39 +0900

    refactor : 어린이 페이지 신규 수정 사항 반영

--- src/app/care/child/core/text.ts
+++ src/app/care/child/core/text.ts
@@ -6,9 +6,6 @@ export const CARE_CHILD = {
 
 export const CARE_CHILDSECTION2 = {
   title: "Kid's AI.DUL",
-  description1: '자녀 성장 멀티 모달 데이터 기반',
-  description2: '연령 맞춤형 성장 촉진 ',
-  description3: '프로그램과 교구를 제공합니다.',
-  imgText1: "Kid's AI.DUL",
-  imgText2: "Parents' AI.DUL",
+  description1: '자녀 성장 멀티 모달 데이터 기반\n 연령 맞춤형 성장 촉진\n 프로그램과 교구를 제공합니다.',
+  description2: 'Kid’s AI.DUL-Parent’s AI.DUL 연동',
 };
